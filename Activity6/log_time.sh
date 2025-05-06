e674e3bfbd0e7d7912ca28a706db6f4a95c7afbd
Arvicss <ramosarvyx@gmail.com>
2025-05-06 16:31:07 +0800

    add edit count

--- Activity6/log_time.sh
+++ Activity6/log_time.sh
@@ -1,1 +1,9 @@
-echo "Current Date and Time: $(date)" >> Activity6/log.txt
+current_date=$(date -Iseconds)
+my_username=$GITHUB_ACTOR
+
+if [ ! -f Activity6/log.txt ]; then
+  echo "Log File Created Current Date and Time: $current_date by $my_username" >> Activity6/log.txt
+else 
+  update_count=$(grep -c 'Logged by' Activity6/log.txt)
+  echo "Log File Updated Current Date and Time: $current_date by $my_username - Update Count: $update_count" >> Activity6/log.txt
+fi
